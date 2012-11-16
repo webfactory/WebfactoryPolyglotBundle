8d2a74322a94f6dfb9ecdab112c774ee435dd2c1
Unknown <unknown@kiln.example.com>
2012-11-16 13:39:36 -0100

    Case 14725 - als Basisklasse fuer Uebersetzungs-Klassen

---
+++ src/Webfactory/Bundle/PolyglotBundle/Entity/BaseTranslation.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Webfactory\Bundle\PolyglotBundle\Entity;
+
+use Webfactory\Bundle\PolyglotBundle\Annotation as Polyglot;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\MappedSuperclass
+ */
+class BaseTranslation {
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column
+     * @Polyglot\Locale
+     */
+    protected $locale;
+
+    /**
+     *  @ORM\JoinColumn(name="entity_id", referencedColumnName="id", nullable="false")
+     */
+    protected $entity;
+
+    public function getLocale() { return $this->locale; }
+
+}
