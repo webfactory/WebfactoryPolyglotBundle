cd00083e3af088ecd18f385d31de1d11fcc0e2a2
Malte Wunsch <mw@webfactory.de>
2014-09-09 10:39:57 +0200

    simplified test entity translation by removing it's heritage

--- src/Webfactory/Bundle/PolyglotBundle/Tests/TestEntityTranslation.php
+++ src/Webfactory/Bundle/PolyglotBundle/Tests/TestEntityTranslation.php
@@ -3,19 +3,42 @@
 namespace Webfactory\Bundle\PolyglotBundle\Tests;
 
 use Doctrine\ORM\Mapping as ORM;
-use Webfactory\Bundle\PolyglotBundle\Entity\BaseTranslation;
+use Webfactory\Bundle\PolyglotBundle\Annotation as Polyglot;
 
 /**
  * Translation Entity of the Doctrine entity that is used for testing.
  *
  * @ORM\Entity
  */
-class TestEntityTranslation extends BaseTranslation
+class TestEntityTranslation
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column
+     * @Polyglot\Locale
+     */
+    protected $locale;
+
+    /**
+     * @ORM\JoinColumn(name="entity_id", referencedColumnName="id", nullable=false)
+     */
+    protected $entity;
+
     /**
      * @ORM\Column(nullable = true)
      */
     public $name;
+
+    public function getLocale()
+    {
+        return $this->locale;
+    }
 
     /**
      * @param string $locale
