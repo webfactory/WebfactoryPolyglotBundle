352e616770d742c3b96f0b849b68ddd877ab83d8
Sebastian Landwehr <sl@webfactory.de>
2016-04-05 10:56:11 +0200

    Test stubs (#1)

---
+++ src/Webfactory/Bundle/PolyglotBundle/Tests/Doctrine/ManagedTranslationProxyTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Webfactory\Bundle\PolyglotBundle\Tests\Doctrine;
+
+
+class ManagedTranslationProxyTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testToStringReturnsTranslatedMessage()
+    {
+
+    }
+
+    public function testToStringReturnsStringIfExceptionOccurredAndNoLoggerIsAvailable()
+    {
+
+    }
+
+    public function testToStringReturnsStringIfExceptionOccurredAndLoggerIsAvailable()
+    {
+
+    }
+
+    public function testToStringLogsExceptionIfLoggerIsAvailable()
+    {
+
+    }
+}
