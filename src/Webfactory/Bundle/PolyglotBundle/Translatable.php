96fb1b6d2b7afee2a21f79fbd2b71906d72e7362
Matthias Molitor <mmo@webfactory.de>
2017-03-09 13:59:09 +0100

    added basic type documentation

--- src/Webfactory/Bundle/PolyglotBundle/Translatable.php
+++ src/Webfactory/Bundle/PolyglotBundle/Translatable.php
@@ -37,15 +37,31 @@ namespace Webfactory\Bundle\PolyglotBundle;
  */
 class Translatable implements TranslatableInterface
 {
+    /**
+     * @var string
+     */
     protected $defaultLocale;
+
+    /**
+     * Maps locales to translations.
+     *
+     * @var array<string, string>
+     */
     protected $translations = array();
 
+    /**
+     * @param string|null $value
+     * @param string|null $defaultLocale
+     */
     public function __construct($value = null, $defaultLocale = null)
     {
         $this->defaultLocale = $defaultLocale ? : '';
         $this->setTranslation($value);
     }
 
+    /**
+     * @param string $locale
+     */
     public function setDefaultLocale($locale)
     {
         if ($this->defaultLocale == '') {
@@ -55,6 +71,10 @@ class Translatable implements TranslatableInterface
         $this->defaultLocale = $locale;
     }
 
+    /**
+     * @param string|null $locale
+     * @return string|null
+     */
     public function translate($locale = null)
     {
         $locale = $locale ? : $this->defaultLocale;
@@ -66,6 +86,10 @@ class Translatable implements TranslatableInterface
         }
     }
 
+    /**
+     * @param string $value
+     * @param string|null $locale
+     */
     public function setTranslation($value, $locale = null)
     {
         $locale = $locale ? : $this->defaultLocale;
@@ -73,11 +97,19 @@ class Translatable implements TranslatableInterface
         $this->translations[$locale] = $value;
     }
 
+    /**
+     * @return string
+     */
     public function __toString()
     {
         return (string)$this->translate();
     }
 
+    /**
+     * Copies translations from this object into the given one.
+     *
+     * @param TranslatableInterface $p
+     */
     public function copy(TranslatableInterface $p)
     {
         foreach ($this->translations as $locale => $value) {
