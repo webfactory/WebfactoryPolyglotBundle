webfactory/WebfactoryPolyglotBundle

commit d4183d82832cc52abb42cf6100b7e018410aca65

Fix a warning when serializing metadata (#9)
This addresses warnings like...

```
Failed to save key "{key}" of type Webfactory\Bundle\PolyglotBundle\Doctrine\TranslatableClassMetadata: Cache key "..." has non-serializable Webfactory\Bundle\PolyglotBundle\Doctrine\TranslatableClassMetadata value.
```

... that occur when the Cache Driver used (in Doctrine) is the file-based implementation from symfony/cache.

src/Doctrine/PersistentTranslatable.php
@@ -9,15 +9,14 @@
 
 namespace Webfactory\Bundle\PolyglotBundle\Doctrine;
 
-use \Doctrine\Common\Collections\Criteria;
+use Doctrine\Common\Collections\Criteria;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
-use \ReflectionProperty;
-use \ReflectionClass;
+use ReflectionClass;
+use ReflectionProperty;
 use Webfactory\Bundle\PolyglotBundle\Exception\TranslationException;
-use Webfactory\Bundle\PolyglotBundle\Translatable;
-use Webfactory\Bundle\PolyglotBundle\TranslatableInterface;
 use Webfactory\Bundle\PolyglotBundle\Locale\DefaultLocaleProvider;
+use Webfactory\Bundle\PolyglotBundle\TranslatableInterface;
 
 /**
  * Eine TranslationProxy-Implementierung für eine Entität, die
@@ -26,19 +25,22 @@ use Webfactory\Bundle\PolyglotBundle\Locale\DefaultLocaleProvider;
 class PersistentTranslatable implements TranslatableInterface
 {
     /**
-     * @var array<string, array<string, object|null>> Cache für die Übersetzungen, indiziert nach Entity-OID und Locale.
-     * Ist static, damit ihn sich verschiedene Proxies (für die gleiche Entität, aber
-     * unterschiedliche Felder) teilen können.
+     * Cache für die Übersetzungen, indiziert nach Entity-OID und Locale. Ist static, damit ihn sich verschiedene Proxies (für die gleiche Entität, aber unterschiedliche Felder) teilen können.
+     *
+     * @var array<string, array<string, object|null>>
      */
-    static protected $_translations = array();
+    protected static $_translations = [];
 
     /**
-     * @var object Die Entität, in der sich dieser Proxy befindet (für die er Übersetzungen verwaltet).
+     * Die Entität, in der sich dieser Proxy befindet (für die er Übersetzungen verwaltet).
+     *
+     * @var object
      */
     protected $entity;
 
     /**
      * Der einzigartige Hash für die verwaltete Entität.
+     *
      * @var string
      */
     protected $oid;
@@ -54,35 +56,44 @@ class PersistentTranslatable implements TranslatableInterface
     protected $primaryValue = null;
 
     /**
-     * @var DefaultLocaleProvider Provider, über den der Proxy die Locale erhält, in der Werte zurückgeben soll, wenn
-     * keine andere Locale explizit gewünscht wird
+     * Provider, über den der Proxy die Locale erhält, in der Werte zurückgeben soll, wenn keine andere Locale explizit gewünscht wird.
+     *
+     * @var DefaultLocaleProvider
      */
     protected $defaultLocaleProvider;
 
     /**
-     * @var ReflectionProperty ReflectionProperty für die Eigenschaft der Translation-Klasse, die den übersetzten Wert
-     * hält
+     * ReflectionProperty für die Eigenschaft der Translation-Klasse, die den übersetzten Wert hält.
+     *
+     * @var ReflectionProperty
      */
     protected $translatedProperty;
 
     /**
-     * @var ReflectionProperty ReflectionProperty für die Eigenschaft der Haupt-Klasse, in der die Übersetzungen als
-     * Doctrine Collection abgelegt sind.
+     * ReflectionProperty für die Eigenschaft der Haupt-Klasse, in der die Übersetzungen als Doctrine Collection abgelegt sind.
+     *
+     * @var ReflectionProperty
      */
     protected $translationCollection;
 
     /**
-     * @var ReflectionClass ReflectionClass für die Klasse, die die Übersetzungen aufnimmt.
+     * ReflectionClass für die Klasse, die die Übersetzungen aufnimmt.
+     *
+     * @var ReflectionClass
      */
     protected $translationClass;
 
     /**
-     * @var ReflectionProperty Das Feld in der Übersetzungs-Klasse, in dem die Locale einer Übersetzung abgelegt ist.
+     * Das Feld in der Übersetzungs-Klasse, in dem die Locale einer Übersetzung abgelegt ist.
+     *
+     * @var ReflectionProperty
      */
     protected $localeField;
 
     /**
-     * @var ReflectionProperty Das Feld in der Übersetzungs-Klasse, in Many-to-one-Beziehung zur Entität abgelegt ist.
+     * Das Feld in der Übersetzungs-Klasse, in Many-to-one-Beziehung zur Entität abgelegt ist.
+     *
+     * @var ReflectionProperty
      */
     protected $translationMapping;
 
@@ -98,19 +109,11 @@ class PersistentTranslatable implements TranslatableInterface
     private $addedTranslations = [];
 
     /**
-     * @param object $entity
-     * @param string $primaryLocale
-     * @param DefaultLocaleProvider $defaultLocaleProvider
-     * @param ReflectionProperty $translatedProperty
-     * @param ReflectionProperty $translationCollection
-     * @param ReflectionClass $translationClass
-     * @param ReflectionProperty $localeField
-     * @param ReflectionProperty $translationMapping
      * @param LoggerInterface $logger
      */
     public function __construct(
-        $entity,
-        $primaryLocale,
+        object $entity,
+        string $primaryLocale,
         DefaultLocaleProvider $defaultLocaleProvider,
         ReflectionProperty $translatedProperty,
         ReflectionProperty $translationCollection,
@@ -118,8 +121,7 @@ class PersistentTranslatable implements TranslatableInterface
         ReflectionProperty $localeField,
         ReflectionProperty $translationMapping,
         LoggerInterface $logger = null
-    )
-    {
+    ) {
         $this->entity = $entity;
         $this->oid = spl_object_hash($entity);
         $this->primaryLocale = $primaryLocale;
@@ -129,7 +131,7 @@ class PersistentTranslatable implements TranslatableInterface
         $this->translationClass = $translationClass;
         $this->localeField = $localeField;
         $this->translationMapping = $translationMapping;
-        $this->logger = ($logger == null) ? new NullLogger() : $logger;
+        $this->logger = (null == $logger) ? new NullLogger() : $logger;
     }
 
     public function setPrimaryValue($value)
@@ -144,11 +146,12 @@ class PersistentTranslatable implements TranslatableInterface
 
     /**
      * @param string $locale
+     *
      * @return object|null
      */
     protected function getTranslationEntity($locale)
     {
-        if ($this->isTranslationCached($locale) === false) {
+        if (false === $this->isTranslationCached($locale)) {
             $this->cacheTranslation($locale);
         }
 
@@ -157,6 +160,7 @@ class PersistentTranslatable implements TranslatableInterface
 
     /**
      * @param string $locale
+     *
      * @return object
      */
     protected function createTranslationEntity($locale)
@@ -176,7 +180,7 @@ class PersistentTranslatable implements TranslatableInterface
     }
 
     /**
-     * @param string $value
+     * @param string      $value
      * @param string|null $locale
      */
     public function setTranslation($value, $locale = null)
@@ -195,7 +199,9 @@ class PersistentTranslatable implements TranslatableInterface
 
     /**
      * @param string|null $locale
+     *
      * @return mixed|string
+     *
      * @throws TranslationException
      */
     public function translate($locale = null)
@@ -212,11 +218,12 @@ class PersistentTranslatable implements TranslatableInterface
                     return $translated;
                 }
             }
+
             return $this->primaryValue;
         } catch (\Exception $e) {
             $message = sprintf(
                 'Cannot translate property %s::%s into locale %s',
-                get_class($this->entity),
+                \get_class($this->entity),
                 $this->translatedProperty->getName(),
                 $locale
             );
@@ -231,7 +238,8 @@ class PersistentTranslatable implements TranslatableInterface
         }
 
         $entity = $this->getTranslationEntity($locale);
-        return $entity && $this->translatedProperty->getValue($entity) !== null;
+
+        return $entity && null !== $this->translatedProperty->getValue($entity);
     }
 
     /**
@@ -240,9 +248,10 @@ class PersistentTranslatable implements TranslatableInterface
     public function __toString()
     {
         try {
-            return (string)$this->translate();
+            return (string) $this->translate();
         } catch (\Exception $e) {
             $this->logger->error($this->stringifyException($e));
+
             return '';
         }
     }
@@ -270,6 +279,7 @@ class PersistentTranslatable implements TranslatableInterface
 
     /**
      * @param string $locale
+     *
      * @return bool
      */
     protected function isTranslationCached($locale)
@@ -298,6 +308,7 @@ class PersistentTranslatable implements TranslatableInterface
 
     /**
      * @param $locale
+     *
      * @return Criteria
      */
     protected function createLocaleCriteria($locale)
@@ -310,6 +321,7 @@ class PersistentTranslatable implements TranslatableInterface
 
     /**
      * @param string $locale
+     *
      * @return object|null
      */
     protected function getCachedTranslation($locale)
@@ -318,19 +330,18 @@ class PersistentTranslatable implements TranslatableInterface
     }
 
     /**
-     * @param \Exception $e
      * @return string
      */
     private function stringifyException(\Exception $e)
     {
         $exceptionAsString = '';
-        while ($e !== null) {
+        while (null !== $e) {
             if (!empty($exceptionAsString)) {
-                $exceptionAsString .= PHP_EOL . 'Previous exception: ' . PHP_EOL;
+                $exceptionAsString .= PHP_EOL.'Previous exception: '.PHP_EOL;
             }
             $exceptionAsString .= sprintf(
                 "Exception '%s' with message '%s' in %s:%d\n%s",
-                get_class($e),
+                \get_class($e),
                 $e->getMessage(),
                 $e->getFile(),
                 $e->getLine(),
@@ -338,6 +349,7 @@ class PersistentTranslatable implements TranslatableInterface
             );
             $e = $e->getPrevious();
         }
+
         return $exceptionAsString;
     }
 }

.coveralls.yml
@@ -1,3 +1,0 @@
-service_name: travis-ci
-src_dir: .
-coverage_clover: build/logs/clover.xml

.github/workflows/dependencies.yml
@@ -1,0 +1,36 @@
+name: Dependencies
+
+on:
+    push:
+        branches:
+            - master
+    pull_request:
+
+env:
+    PHP_VERSION: 7.2
+
+jobs:
+    composer-require-checker:
+        name: Check missing composer requirements
+        runs-on: ubuntu-18.04
+        steps:
+            -   uses: actions/checkout@v2
+            -   name: Konfiguriere PHP-Version und -Einstellungen im Worker-Node
+                uses: shivammathur/setup-php@v2
+                with:
+                  php-version: ${{ env.PHP_VERSION }}
+                  tools: composer:v2
+                  ini-values: variables_order=EGPCS
+            -   name: Cache Composer Dependencies
+                uses: actions/cache@v1
+                with:
+                    path: vendor/
+                    key: composer-${{ env.PHP_VERSION }}-${{ hashFiles('composer.*') }}
+                    restore-keys: |
+                        composer-${{ env.PHP_VERSION }}-${{ github.ref }}
+                        composer-${{ env.PHP_VERSION }}-
+            -   run: |
+                    composer install --no-interaction --no-scripts --no-progress --no-suggest
+                    composer show
+            -   name: ComposerRequireChecker
+                uses: docker://webfactory/composer-require-checker:2.1.0

.github/workflows/fix-cs-php.yml
@@ -1,0 +1,50 @@
+# Update this by running
+# curl https://gist.githubusercontent.com/mpdude/ca93a185bcbf56eb7e341632ad4f8263/raw/fix-cs-php.yml > .github/workflows/fix-cs-php.yml
+
+on:
+    push:
+        branches:
+            - master
+    pull_request:
+
+name: Coding Standards
+
+jobs:
+    open-pr-for-cs-violations:
+        name: PHP-CS-Fixer
+        runs-on: ubuntu-18.04
+        steps:
+
+            -   name: Checkout code
+                uses: actions/checkout@v2
+                with:
+                    ref: ${{ github.head_ref }}
+
+            -   name: Run PHP-CS-Fixer
+                uses: docker://oskarstark/php-cs-fixer-ga:2.16.7
+
+            -   name: Create PR for CS fixups
+                uses: peter-evans/create-pull-request@c7f493a8000b8aeb17a1332e326ba76b57cb83eb
+                id: create-pull-request
+                with:
+                    token: ${{ secrets.GITHUB_TOKEN }}
+                    title: Fix coding standards in ${{ github.ref }}
+                    branch: php-cs-fixer/${{ github.ref }}
+                    assignees: ${{ github.actor }}
+                    labels: php-cs-fixer
+                    body: Please merge these changes into the ${{ github.ref }} branch to fix coding standard violations.
+                    commit-message: Apply php-cs-fixer changes as of ${{ github.sha }}
+
+            -   name: Leave a notice in the discussion when fixing code in a Pull Request
+                uses: docker://mpdude/comment-on-pr:v1.2.0
+                env:
+                    GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+                if: steps.create-pull-request.outputs.pr_number # only if PR was created in the previous step
+                continue-on-error: true # continue on failure - necessary when the current branch does not have a pending PR
+                with:
+                    msg: |
+                        @${{ github.actor }} please apply the coding standard fixes from #${{ steps.create-pull-request.outputs.pr_number }}
+
+            -   name: Fail the workflow when necessary CS fixes were detected
+                run: echo "Failing workflow run because CS violations were detected." && exit 1
+                if: steps.create-pull-request.outputs.pr_number

.github/workflows/tests.yml
@@ -1,0 +1,35 @@
+name: Tests
+
+on:
+    push:
+        branches:
+            - master
+    pull_request:
+
+env:
+    PHP_VERSION: 7.2
+
+jobs:
+    PHPUnit:
+        name: PHPUnit
+        runs-on: ubuntu-18.04
+        steps:
+            -   uses: actions/checkout@v2
+            -   name: Konfiguriere PHP-Version und -Einstellungen im Worker-Node
+                uses: shivammathur/setup-php@v2
+                with:
+                  php-version: ${{ env.PHP_VERSION }}
+                  tools: composer:v2
+                  ini-values: variables_order=EGPCS
+            -   name: Cache Composer Dependencies
+                uses: actions/cache@v1
+                with:
+                    path: vendor/
+                    key: composer-${{ env.PHP_VERSION }}-${{ hashFiles('composer.*') }}
+                    restore-keys: |
+                        composer-${{ env.PHP_VERSION }}-${{ github.ref }}
+                        composer-${{ env.PHP_VERSION }}-
+            -   run: |
+                    composer install --no-interaction --no-scripts --no-progress --no-suggest
+                    composer show
+            -   run: vendor/bin/phpunit

.gitignore
@@ -1,3 +1,4 @@
 vendor/
-.idea/
 composer.lock
+phpunit.xml$
+.php_cs.cache

.php_cs.dist
@@ -1,0 +1,19 @@
+<?php
+
+return PhpCsFixer\Config::create()
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'array_syntax' => array('syntax' => 'short'),
+        'no_unreachable_default_argument_value' => false,
+        'braces' => array('allow_single_line_closure' => true),
+        'heredoc_to_nowdoc' => false,
+        'phpdoc_annotation_without_dot' => false,
+    ])
+    ->setRiskyAllowed(true)
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->in(__DIR__)
+            ->notPath('vendor/')
+    )
+;

.scrutinizer.yml
@@ -1,17 +1,0 @@
-build:
-    environment:
-        php: '7.2'
-        timezone: 'Europe/Berlin'
-        postgresql: false
-        node: false
-        redis: false
-        variables:
-            COMPOSER_MEMORY_LIMIT: "-1"
-
-    tests:
-        override:
-            -
-                command: 'vendor/bin/phpunit --coverage-clover=clover.xml'
-                coverage:
-                    file: 'clover.xml'
-                    format: 'clover'

commit a5798eec2ef778a4618fcaa18543339a29ed6d2d

Fix a regression introduced in d4183d8

src/Doctrine/PersistentTranslatable.php
@@ -113,7 +113,7 @@ class PersistentTranslatable implements TranslatableInterface
      */
     public function __construct(
         object $entity,
-        string $primaryLocale,
+        ?string $primaryLocale,
         DefaultLocaleProvider $defaultLocaleProvider,
         ReflectionProperty $translatedProperty,
         ReflectionProperty $translationCollection,