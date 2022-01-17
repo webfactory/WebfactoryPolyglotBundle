c9700aac9d34ed4fdbf491bb94eda247972c4697
Matthias Pigulla <mp@webfactory.de>
2022-01-17 09:14:19 +0000

    Avoid pinning symfony/phpunit-bridge

--- .github/workflows/lock-symfony-version.sh
+++ .github/workflows/lock-symfony-version.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sed --in-place --regexp-extended --expression='/symfony\/error-handler/! s/"(symfony\/.*)": ".*"/"\1": "'$VERSION'"/' composer.json
+sed --in-place --regexp-extended --expression='/symfony\/phpunit-bridge/! /symfony\/error-handler/! s/"(symfony\/.*)": ".*"/"\1": "'$VERSION'"/' composer.json
