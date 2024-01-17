03fd53baf9229f2af5a275c6901e24c60159ae0a
Matthias Pigulla <mp@webfactory.de>
2024-01-17 19:25:26 +0100

    Add a `TranslatableChain` (#32)
    Add a `TranslatableChain` object that can pick translations from a
    series of `TranslatableInterface` instances.
    
    This is useful e.g. when you have "fallback" objects or data and want to
    display their data in case no translation is available on a "base"
    object.

---
+++ src/TranslatableChain.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Webfactory\Bundle\PolyglotBundle;
+
+/**
+ * Chain of multiple translatable objects.
+ *
+ * Goes through a list of `TranslatableInterface` instances and returns the first
+ * (optionally, non-empty) translation found. Updates are passed to the primary translatable.
+ */
+final class TranslatableChain implements TranslatableInterface
+{
+    /**
+     * @var list<TranslatableInterface>
+     */
+    private $translatables;
+
+    private $comparator;
+
+    public static function firstNonEmpty(...$translatables): self
+    {
+        return new self(function ($value) {
+            return null !== $value && '' !== trim($value);
+        }, ...$translatables);
+    }
+
+    public static function firstTranslation(...$translatables): self
+    {
+        return new self(function ($value) {
+            return null !== $value;
+        }, ...$translatables);
+    }
+
+    private function __construct($comparator, ...$translatables)
+    {
+        $this->comparator = $comparator;
+        $this->translatables = $translatables;
+    }
+
+    public function translate(string $locale = null)
+    {
+        $c = $this->comparator;
+        foreach ($this->translatables as $translation) {
+            $value = $translation->translate($locale);
+            if ($c($value)) {
+                return $value;
+            }
+        }
+
+        return null;
+    }
+
+    public function setTranslation($value, string $locale = null): void
+    {
+        $this->translatables[0]->setTranslation($value, $locale);
+    }
+
+    public function isTranslatedInto($locale): bool
+    {
+        foreach ($this->translatables as $translation) {
+            if ($translation->isTranslatedInto($locale)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function __toString(): string
+    {
+        return $this->translate();
+    }
+}
