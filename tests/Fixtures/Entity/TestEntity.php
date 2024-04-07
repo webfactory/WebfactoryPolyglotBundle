91a74ea62522327959cff502de216ce80150993e
Matthias Pigulla <mp@webfactory.de>
2024-04-07 15:18:09 +0200

    Integration tests with the Symfony full-stack framework (backport to 3.x)
    This backports the changes from #54 onto the 3.x branch.

--- tests/Fixtures/Entity/TestEntity.php
+++ tests/Fixtures/Entity/TestEntity.php
@@ -7,7 +7,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Webfactory\Bundle\PolyglotBundle\Tests;
+namespace Webfactory\Bundle\PolyglotBundle\Tests\Fixtures\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
