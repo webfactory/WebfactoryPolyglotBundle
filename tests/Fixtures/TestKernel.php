0b71665a4de8a238ed118fa5c191aa547c3f8dd9
Matthias Pigulla <mp@webfactory.de>
2025-12-15 17:35:58 +0100

    Bump requirements to currently supported Symfony versions, allow 8.x to be used (#88)
    In order to get a test Kernel with a configuration that is compatible
    across Symfony 6.4 to 8.0, we need to switch to PHP as the configuration
    mechanism so that we can conditionally include the
    `framework.annotations` configuration setting.
    
    (Case 204755)

--- tests/Fixtures/TestKernel.php
+++ tests/Fixtures/TestKernel.php
@@ -2,7 +2,11 @@
 
 namespace Webfactory\Bundle\PolyglotBundle\Tests\Fixtures;
 
+use Doctrine\DBAL\Logging\Middleware;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ChildDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\Kernel;
 
 class TestKernel extends Kernel
@@ -18,7 +22,45 @@ class TestKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader): void
     {
-        $loader->load(__DIR__.'/config/config.yml');
+        $loader->load(static function (ContainerBuilder $container): void {
+            // Framework configuration
+            $container->loadFromExtension('framework', [
+                'test' => true,
+            ] + (Kernel::VERSION_ID < 70000 ? ['annotations' => ['enabled' => false]] : []));
+
+            // Webfactory Polyglot Bundle configuration
+            $container->loadFromExtension('webfactory_polyglot', [
+                'defaultLocale' => 'en_GB',
+            ]);
+
+            // Doctrine configuration
+            $container->loadFromExtension('doctrine', [
+                'dbal' => [
+                    'driver' => 'pdo_sqlite',
+                    'memory' => true,
+                ],
+                'orm' => [
+                    'mappings' => [
+                        'WebfactoryPolyglotBundle' => [
+                            'type' => 'attribute',
+                            'dir' => '../tests/Fixtures/Entity',
+                            'prefix' => 'Webfactory\Bundle\PolyglotBundle\Tests\Fixtures\Entity',
+                        ],
+                    ],
+                ],
+            ]);
+
+            // Service definitions
+            $container->setDefinition('entity_manager_for_loading', new ChildDefinition('doctrine.orm.entity_manager'));
+
+            $container->register(QueryLogger::class);
+
+            $container->register(Middleware::class)
+                ->setArguments([
+                    '$logger' => new Reference(QueryLogger::class),
+                ])
+                ->addTag('doctrine.middleware');
+        });
     }
 
     public function getProjectDir(): string
